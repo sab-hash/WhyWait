3e927597411209c75f77de7b96b04f8f7d048210
Victory Bedru <victorybedru@gmail.com>
2026-08-17 04:17:02 -0700

    feat: add taxi JSON serialization

--- frontend/lib/features/passenger/live_tracking_screen.dart
+++ frontend/lib/features/passenger/live_tracking_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
+// MODEL
 enum TaxiStatus { available, filling }
 
 class Taxi {
@@ -24,4 +25,32 @@ class Taxi {
     required this.latitude,
     required this.longitude,
   });
+
+  factory Taxi.fromJson(Map<String, dynamic> json) {
+    return Taxi(
+      id: json['id'].toString(),
+      name: json['name'] as String,
+      plate: json['plate'] as String,
+      distanceKm: (json['distance_km'] as num).toDouble(),
+      etaMinutes: json['eta_minutes'] as int,
+      status: (json['status'] as String) == 'available'
+          ? TaxiStatus.available
+          : TaxiStatus.filling,
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'plate': plate,
+      'distance_km': distanceKm,
+      'eta_minutes': etaMinutes,
+      'status': status == TaxiStatus.available ? 'available' : 'filling',
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
 }
